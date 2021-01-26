dcce53346cc8f0a7aa0521a6099ac523f99fcc36
Viktor Vashchenko <viktor2236@gmail.com>
2021-01-26 13:07:30 +0200

    update

--- src/js/themes.js
+++ src/js/themes.js
@@ -7,37 +7,47 @@ const { LIGHT, DARK } = Theme;
 const checkBoxRef = document.querySelector('.theme-switch__toggle');
 const bodyRef = document.querySelector('body');
 
+const savedTheme = localStorage.getItem('Theme');
+
 bodyRef.classList.add(LIGHT);
 
 function changeTheme(event) {
   if (checkBoxRef.checked) {
     bodyRef.classList.replace(LIGHT, DARK);
+    localStorage.setItem('Theme', DARK);
   } else {
     bodyRef.classList.replace(DARK, LIGHT);
+    localStorage.setItem('Theme', LIGHT);
   }
 }
+
+if (savedTheme === DARK) {
+  bodyRef.classList.replace(LIGHT, DARK);
+  checkBoxRef.checked = true;
+} 
 
 checkBoxRef.addEventListener('change', changeTheme);
 
 
 // local Storage
 
-const savedMessage = localStorage.getItem('Theme');
 
-checkBoxRef.addEventListener('change', saveToLocal);
 
-if (savedMessage === DARK ) {
-  bodyRef.classList.replace(LIGHT, DARK);
-  checkBoxRef.checked = true;
-} 
 
-function saveToLocal(event) {
-  if (event.target.checked) {
-    localStorage.setItem('Theme', DARK);
-  } else {
-    localStorage.setItem('Theme', LIGHT);
-  }
-}
+
+
+
+//checkBoxRef.addEventListener('change', saveToLocal);
+
+
+
+// function saveToLocal(event) {
+//   if (event.target.checked) {
+//     localStorage.setItem('Theme', DARK);
+//   } else {
+//     localStorage.setItem('Theme', LIGHT);
+//   }
+// }
 
 
 // function changeToDarkTheme() {
